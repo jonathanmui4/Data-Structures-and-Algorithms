3333672de5bc9d42e11a6438fe016343042eb4cc
Jonathan Mui <jonathanmui4@gmail.com>
2024-04-01 10:26:47 +0800

    Add Truck Tour

---
+++ Problem_Sets/HackerRank/TruckTour/Readme.md
@@ -0,0 +1,19 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+Iteration problem.
+
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+Iterate through array. Hard code starting index to 0, same as where the loop starts. For each petrol station, calculate if petrol remaining in the car is negative. If it is, starting index is incremented to i + 1. Else, continue with the loop
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+Iterate through array
+$$O(n)$$
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+Additional Data Structures used take up constant space
+$$O(1)$$
---
+++ Problem_Sets/HackerRank/TruckTour/TruckTour.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'truckTour' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+#
+
+def truckTour(petrolpumps):
+    # Write your code here
+    start, remPetrol = 0, 0
+    for i in range(len(petrolpumps)):
+        petrol, dist = petrolpumps[i]
+        remPetrol += petrol - dist
+        if remPetrol < 0:
+            start = i + 1
+            remPetrol = 0
+    return start
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    petrolpumps = []
+
+    for _ in range(n):
+        petrolpumps.append(list(map(int, input().rstrip().split())))
+
+    result = truckTour(petrolpumps)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
